971a8a3ece443946cc1541ae36cf8d7686edf250
Methus Narksenee <methdevth@gmail.com>
2018-12-11 12:48:34 +0700

    update base ai

--- Model.js
+++ Model.js
@@ -6,10 +6,12 @@ const p2_selectedCards = [ [0],[1] ]
 const highestCardValue = 3
 const isOdd = true
 
+// Ai Playing Card
 const getAISelectedCards = (currentDeckP1, currentDeckP2, p1_selectedCards, highestCardValue, isOdd) => {
     dosomething()
 }
 
+// Check Win State
 const is_win = (state) => {
     if (state.length == 0) {
         return true
@@ -17,6 +19,73 @@ const is_win = (state) => {
     return false
 }
 
+const SortArray = (data) => {
+    var buffer = []
+    for(let i = 0; i < data.length;i++) {
+        for (let j = 0; j < data[i].length;j++) {
+            buffer.push(data[i][j])
+        }
+    }
+    return buffer
+}
+
+// Run Expand
+const allExpand = (data) => {
+    var selectedAllPossible = []
+    var x = GroupNode(data)
+    for(let i = 0;i < x.length;i++) {
+       selectedAllPossible.push(filterUndefined(ExpandPossibleNode(x[i])))
+    }
+    return selectedAllPossible
+}
+
+// Filter Array
+const filterUndefined = (selectcard) => {
+    var filter = []
+    for (let j = 0; j < selectcard.length; j++) {
+        if (selectcard[j].includes(undefined) == false && selectcard[j].length != 0){
+            filter.push(selectcard[j])
+        }
+    }
+    return filter
+}
+
+// Expand Card Based on Type of Card
+const ExpandPossibleNode = (data) => {
+    index = [[0],[1],[2],[3],[0,1],[0,2],[0,3],[1,2],[1,3],[2,3],
+            [0,1,2],[0,1,3],[0,2,3],[1,2,3],
+            [0,1,2,3]]
+    selectedCard = []
+    for(let i = 0; i < index.length;i++) {
+        buffer = []
+        for (let j = 0; j < index[i].length; j++) {
+            if (data.length >= index[i].length) {
+                buffer.push(data[index[i][j]])
+            }
+        }
+        selectedCard.push(buffer)
+    }
+
+    return selectedCard
+}
+
+// Group The Same Number Card
+const GroupNode = (data) => {
+    var buffer = []
+    var groupNode = []
+    for(let i = 0; i < data.length;i++) {
+        if (parseInt((data[i]+4)/4) == parseInt((data[i+1]+4)/4)){
+            buffer.push(data[i])
+        } else {
+            buffer.push(data[i])
+            groupNode.push(buffer)
+            buffer = []
+        }
+    }
+    return groupNode
+}
+
+// List All Possible Card
 const getPossibleCard = (p1SelectedCard,p2SelectedCard,p2Card,isOdd) => {
     var PossibleCardP2 = []
     for(let i = 0; i < p2SelectedCard.length;i++) {
@@ -27,6 +96,7 @@ const getPossibleCard = (p1SelectedCard,p2SelectedCard,p2Card,isOdd) => {
     return PossibleCardP2
 }
 
+// Check Condition Playing Card 
 const isSelectedCardCanPlay = (p1SelectedCard,p2SelectedCard) => {
     for(let i = 0; i < p1SelectedCard.length;i++) {
         var maxTypeCard
@@ -55,6 +125,7 @@ const isSelectedCardCanPlay = (p1SelectedCard,p2SelectedCard) => {
     return true
 }
 
+// Validate Even turn or Odd turn
 const validateSelectedCards = (isOdd, selectedCards, currentDeck) => {
   let isError = false;
   if (isOdd != null) {
